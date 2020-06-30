12b40b8907c91b1eb9002ec364fc41fc9167cc77
manchandajayant <maanjayant@gmail.com>
2020-06-30 18:17:11 +0200

    hooks

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,79 +1,55 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { showAllSpaces } from "../actions/spaceActions";
 import { Typography } from "@material-ui/core";
 
-// const styles = {
-//   root: {
-//     display: "flex",
-//     flexWrap: "nowrap",
-//     justifyContent: "space-around",
-//     overflow: "hidden",
-//   },
-//   gridList: {
-//     width: 1200,
-//     height: 1100,
-//     paddingLeft: "1.5%",
-//   },
-//   icon: {
-//     color: "#fff",
-//   },
-// };
+const Homepage = () => {
+  const dispatch = useDispatch();
+  const spaces = useSelector((state) => state.spaces);
+  useEffect(() => {
+    dispatch(showAllSpaces());
+  }, [dispatch]);
+  console.log("ssp", spaces);
 
-export class Homepage extends Component {
-  componentDidMount() {
-    this.props.showAllSpaces();
-  }
-
-  render() {
-    // console.log(this.props);
-    if (!this.props.spaces) {
-      return <h1>Loading...</h1>;
-    } else {
-      return (
-        <div>
-          <Typography
-            variant="h4"
-            style={{
-              color: "black",
-              fontWeight: "2px",
-              fontFamily: "IBM Plex Serif,serif",
-            }}
-          >
-            SPACES
-          </Typography>
-          {this.props.spaces.map((space, index) => (
-            <div key={index}>
-              <img
-                src={space.url}
-                alt="Not loading"
-                style={{
-                  filter: "sepia(100%)",
-                }}
-              />
-
-              <Typography>
-                <Link to={`/spaces/${space.id}`}>{space.name}</Link>
-              </Typography>
+  if (!spaces) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <div>
+        <Typography
+          variant="h4"
+          style={{
+            color: "black",
+            fontWeight: "2px",
+            fontFamily: "IBM Plex Serif,serif",
+          }}
+        >
+          SPACES
+        </Typography>
+        {spaces.map((space, index) => (
+          <div key={index} className="container">
+            <div className="row">
+              <div className="col">
+                {" "}
+                <img
+                  src={space.url}
+                  alt="Not loading"
+                  style={{
+                    filter: "sepia(100%)",
+                  }}
+                />
+              </div>
             </div>
-          ))}{" "}
-        </div>
-      );
-    }
+
+            <Typography>
+              <Link to={`/spaces/${space.id}`}>{space.name}</Link>
+            </Typography>
+          </div>
+        ))}{" "}
+      </div>
+    );
   }
-}
-// Homepage.propTypes = {
-//   classes: PropTypes.object.isRequired,
-// };
-
-const mapStateToProps = (state) => ({
-  spaces: state.spaces,
-  user: state.users,
-});
-
-const mapDispatchToProps = {
-  showAllSpaces,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Homepage);
+export default Homepage;
