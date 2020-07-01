4dd0eee7ae4b59694ebf6db170dccadce2d90a0a
manchandajayant <maanjayant@gmail.com>
2020-07-01 14:55:49 +0200

    hooks

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,10 +3,25 @@ import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { showAllSpaces } from "../actions/spaceActions";
 import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const Homepage = () => {
   const dispatch = useDispatch();
   const spaces = useSelector((state) => state.spaces);
+  const classes = useStyles();
   useEffect(() => {
     dispatch(showAllSpaces());
   }, [dispatch]);
@@ -28,20 +43,20 @@ const Homepage = () => {
           SPACES
         </Typography>
         {spaces.map((space, index) => (
-          <div key={index} className="container">
-            <div className="row">
-              <div className="col">
-                {" "}
-                <img
-                  src={space.url}
-                  alt="Not loading"
-                  style={{
-                    filter: "sepia(100%)",
-                  }}
-                />
-              </div>
-            </div>
-
+          <div key={index} className={classes.root}>
+            <Grid item xs={12} sm={6}>
+              <img
+                src={space.url}
+                alt="Not loading"
+                style={{
+                  filter: "sepia(100%)",
+                  height: "auto",
+                  width: "100%",
+                  maxWidth: "500px",
+                }}
+                className={classes.paper}
+              />
+            </Grid>{" "}
             <Typography>
               <Link to={`/spaces/${space.id}`}>{space.name}</Link>
             </Typography>
