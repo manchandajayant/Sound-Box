04feedb35beb20e6d9cf3269c1a1d6a0e168aaae
manchandajayant <maanjayant@gmail.com>
2020-07-03 00:53:00 +0200

    adding a map

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { showAllSpaces } from "../actions/spaceActions";
-import { Typography } from "@material-ui/core";
+// import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
+// import Paper from "@material-ui/core/Paper";
+// import Grid from "@material-ui/core/Grid";
+import ReactMapGl from "react-map-gl";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,6 +21,13 @@ const useStyles = makeStyles((theme) => ({
 
 const Homepage = () => {
   const dispatch = useDispatch();
+  const [viewPort, setviewPort] = useState({
+    latitude: 45.11,
+    longitude: -75.6903,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10,
+  });
   const spaces = useSelector((state) => state.spaces);
   const classes = useStyles();
   useEffect(() => {
@@ -32,37 +40,40 @@ const Homepage = () => {
   } else {
     return (
       <div>
-        <Typography
-          variant="h4"
-          style={{
-            color: "black",
-            fontWeight: "2px",
-            fontFamily: "IBM Plex Serif,serif",
-          }}
-        >
-          SPACES
-        </Typography>
-        {spaces.map((space, index) => (
-          <div key={index} className={classes.root}>
-            <Grid item xs={12} sm={6}>
-              <img
-                src={space.url}
-                alt="Not loading"
-                style={{
-                  filter: "sepia(100%)",
-                  height: "auto",
-                  width: "100%",
-                  maxWidth: "500px",
-                }}
-                className={classes.paper}
-              />
-            </Grid>{" "}
-            <Typography>
-              <Link to={`/spaces/${space.id}`}>{space.name}</Link>
-            </Typography>
-          </div>
-        ))}{" "}
+        <h1>Map</h1>
       </div>
+      // <div>
+      //   <Typography
+      //     variant="h4"
+      //     style={{
+      //       color: "black",
+      //       fontWeight: "2px",
+      //       fontFamily: "IBM Plex Serif,serif",
+      //     }}
+      //   >
+      //     SPACES
+      //   </Typography>
+      //   {spaces.map((space, index) => (
+      //     <div key={index} className={classes.root}>
+      //       <Grid item xs={12} sm={6}>
+      //         <img
+      //           src={space.url}
+      //           alt="Not loading"
+      //           style={{
+      //             filter: "sepia(100%)",
+      //             height: "auto",
+      //             width: "100%",
+      //             maxWidth: "500px",
+      //           }}
+      //           className={classes.paper}
+      //         />
+      //       </Grid>{" "}
+      //       <Typography>
+      //         <Link to={`/spaces/${space.id}`}>{space.name}</Link>
+      //       </Typography>
+      //     </div>
+      //   ))}{" "}
+      // </div>
     );
   }
 };
