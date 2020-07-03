62b74fd6501445e1dfba4e9670168e4c6da4b620
manchandajayant <maanjayant@gmail.com>
2020-07-03 15:46:52 +0200

    interactive map

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,11 +3,18 @@ import { useSelector, useDispatch } from "react-redux";
 import { showAllSpaces } from "../actions/spaceActions";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import { Icon } from "leaflet";
+import { Link } from "react-router-dom";
 import "../CSS/Homepage.css";
+
+const iconSpace = new Icon({
+  iconUrl: "/historic-theatre.png",
+  iconSize: [20, 20],
+});
 
 const Homepage = () => {
   const dispatch = useDispatch();
   const spaces = useSelector((state) => state.spaces);
+  const [activeSpace, setactiveSpace] = useState(null);
 
   useEffect(() => {
     dispatch(showAllSpaces());
@@ -19,8 +26,8 @@ const Homepage = () => {
   } else {
     return (
       <div>
-        <h1>Map</h1>
-        <Map center={[49.185294, 6.502752]} zoom={3}>
+        <h1>Spaces</h1>
+        <Map center={[56.992882804633986, 10.04150390625]} zoom={4}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"
@@ -30,9 +37,26 @@ const Homepage = () => {
               <Marker
                 key={coord.id}
                 position={[coord.latitude, coord.longitude]}
+                onclick={() => {
+                  setactiveSpace(coord);
+                }}
+                icon={iconSpace}
               />
             );
           })}
+          {activeSpace && (
+            <Popup
+              position={[activeSpace.latitude, activeSpace.longitude]}
+              onClose={() => {
+                setactiveSpace(null);
+              }}
+            >
+              <Link to={`/spaces/${activeSpace.id}`}>
+                <h2>{activeSpace.name}</h2>
+                <img src="/microphone.png" />
+              </Link>
+            </Popup>
+          )}
         </Map>
       </div>
     );
