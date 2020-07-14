51e6e673607e84837671e28b43f199bd82a63061
manchandajayant <maanjayant@gmail.com>
2020-07-14 15:59:18 +0200

    upload bar

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import ReactPlayer from "react-player";
 import { withStyles } from "@material-ui/core/styles";
 import GitHubIcon from "@material-ui/icons/GitHub";
-import AlternateEmailIcon from "@material-ui/icons/AlternateEmail";
+//import AlternateEmailIcon from "@material-ui/icons/AlternateEmail";
 import IconButton from "@material-ui/core/IconButton";
 import PropTypes from "prop-types";
 import Link from "@material-ui/core/Link";
@@ -44,8 +44,8 @@ export class About extends Component {
           places, mostly through openair library recorded by researchers at
           university of york. As things get normal i intend to record impulse
           responses for more sites and catalogue them. Incase you you want to
-          contribute with any impulse response data for any space, then you
-          could mail me and i will accredit you here on the page
+          contribute with any impulse response data for a space, then you could
+          mail me and i will accredit you here.
           <br />
           <br />
           If you are interested in creating your own impulse responses, here is
