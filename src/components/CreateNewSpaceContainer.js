ff9160048a8e2adc0733d7b766a3a9aa92e3d355
manchandajayant <maanjayant@gmail.com>
2020-07-23 10:39:02 +0200

    rrefa

--- src/components/CreateNewSpaceContainer.js
+++ src/components/CreateNewSpaceContainer.js
@@ -8,7 +8,6 @@ import { Link } from "react-router-dom";
 import { Typography, TextField, Button } from "@material-ui/core";
 import LoginFormContainer from "./LoginFormContainer";
 import Map from "./Map";
-//import Map2 from "./Map2";
 
 class CreateNewSpaceContainer extends Component {
   state = {
@@ -208,9 +207,8 @@ class CreateNewSpaceContainer extends Component {
             <Link to="/about">About</Link> page
           </Typography>
           <br />
-          {/* <Map place={this.addAPlace} /> */}
-          <Map />
-          {/* <Map2 /> */}
+          <Map place={this.addAPlace} />
+
           <br />
           <br />
           <CreateNewSpace
